111f7e69d33057a20773c6b02754e13d487fc3b9
kietng04 <124132816+kietng04@users.noreply.github.com>
2024-05-08 10:42:18 +0700

    a

--- js/importproduct.js
+++ js/importproduct.js
@@ -2,6 +2,7 @@ loadProductPN();
 addeventThempn();
 loadmaphieunhap();
 addeventsuapn();
+addeventxoapn();
 var curProduct;
 var listDeProduct = [];
 var listDeLength = 0;
@@ -247,6 +248,71 @@ function addeventsuapn() {
             createToast('error', 'Vui lòng chọn sản phẩm cần sửa!');
             return;
         }    
+        // sua
+        var index;
+        document.querySelectorAll('.rowdetail').forEach((element, i) => {
+            if (element.classList.contains('black')) {
+                index = i;
+            }
+        })
+
+        var masp = document.querySelector('#product-name').value.split(' - ')[0];
+        var tensp = document.querySelector('#product-name').value.split(' - ')[1]
+        var masize = document.querySelector('#product-item').value[0];
+        var made = document.querySelector('#product-item').value[1];
+        var soluong = document.querySelector('#product-quantity').value;
+        var giaban = document.querySelector('#product-pricesell').value;
+        var gianhap = document.querySelector('#product-price').value;
+        if (masp == '' || masize == '' || made == '' || soluong == '') {
+            createToast('error', 'Vui lòng nhập đủ thông tin phiếu nhập!');
+            return;
+        }
+
+        if (parseFloat(gianhap) >= parseFloat(giaban)) {
+            createToast('error', 'Giá nhập phải nhỏ hơn giá bán!');
+            return;
+        }
+
+        listCTPN[index] = {
+            "MaSP": masp,
+            "TenSP": tensp,
+            "MaSize": masize,
+            "MaDe": made,
+            "SoLuong": soluong,
+            "TenSize": document.querySelector('#product-item').options[document.querySelector('#product-item').selectedIndex].text.split(' - ')[0],
+            "TenDe": document.querySelector('#product-item').options[document.querySelector('#product-item').selectedIndex].text.split(' - ')[1],
+            "GiaNhap": document.getElementById('product-price').value,
+            "GiaBan": document.getElementById('product-pricesell').value
+        }
+        loadTablepn();
     })
 
+}
+
+
+
+function addeventxoapn() {
+    document.querySelector('.btn-control-large.item-3').addEventListener('click', function(e) {
+        e.preventDefault();
+        var flag = 0;
+        document.querySelectorAll('.rowdetail').forEach(element => {
+            if (element.classList.contains('black')) {
+                flag = 1;
+            }
+        })
+        if (flag == 0) {
+            createToast('error', 'Vui lòng chọn sản phẩm cần xóa!');
+            return;
+        }
+        var index;
+        document.querySelectorAll('.rowdetail').forEach((element, i) => {
+            if (element.classList.contains('black')) {
+                index = i;
+            }
+        })
+        sum -= parseInt(listCTPN[index].GiaNhap) * parseInt(listCTPN[index].SoLuong);
+        document.querySelector('.tongtienpn').innerHTML = toVND(sum);
+        listCTPN.splice(index, 1);
+        loadTablepn();
+    })
 }

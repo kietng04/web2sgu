da42958ac566a91950e12c1869eb7f31fb045a2d
TKieen <nguyenthekien62@gmail.com>
2024-05-16 14:01:15 +0700

    thêm thongTinNguoiDungBUS vào load

--- controller/ProductsController.php
+++ controller/ProductsController.php
@@ -5,6 +5,7 @@
 require_once('BaseController.php');
 require_once(__DIR__ . '/../model/NguoiDungBUS.php');
 require_once(__DIR__ . '/../model/SanPhamBUS.php');
+require_once(__DIR__ . '/../model/NhanVienBUS.php');
 
 session_start();
 
